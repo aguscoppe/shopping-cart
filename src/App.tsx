2c116654e9edfd2e5c7b7c16d70d278a48c7d781
Agustina Coppe <agustina.coppe@endava.com>
2021-12-28 20:48:28 -0300

    Create basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,49 @@
+import { useState } from "react";
+import { useQuery } from "react-query";
+// Components
+import Item from "./Item/Item";
+import { Drawer, LinearProgress, Grid, Badge } from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+// Styles
+import { Wrapper } from "./App.styles";
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch("https://fakestoreapi.com/products")).json();
+
+const getTotalItems = () => null;
+
+const handleAddToCart = (clickedItem: CartItemType) => null;
+
+const handleRemoveFromCart = () => null;
+
 const App = () => {
-  return <div className="App">Start</div>;
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong!</div>;
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 };
 
 export default App;
